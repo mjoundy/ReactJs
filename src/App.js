b0c6fb7f2a3374e8454304c7363d80b95997f7ba
mariem <joundy.mariem@gmail.com>
2024-01-17 13:29:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bigcontainer">
+      <div className="container">
+        <div id="photo">
+          
+        </div>
+       
+        <h1>Mariem Joundy</h1>
+        <h3>student test test</h3>
+        <h3>html-css-reactjs</h3>
+      </div>
+      <div className="container">
+        <div id="photo">
+          
+        </div>
+       
+        <h1>Mariem Joundy</h1>
+        <h3>student test test</h3>
+        <h3>html-css-reactjs</h3>
+      </div>
+      <div className="container">
+        <div id="photo">
+          
+        </div>
+       
+        <h1>Mariem Joundy</h1>
+        <h3>student test test</h3>
+        <h3>html-css-reactjs</h3>
+      </div>
+      <div className="container">
+        <div id="photo">
+          
+        </div>
+       
+        <h1>Mariem Joundy</h1>
+        <h3>student test test</h3>
+        <h3>html-css-reactjs</h3>
+      </div>
     </div>
   );
 }
